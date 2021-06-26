d9fbd9aa6d7e6f2bab5a498fa762d13abd4c2f59
Jim Marsden <jamespmarsden@hotmail.com>
2021-06-26 04:09:51 -0700

    renamed the test for consistency and cleaned up the tests, minimizing overlap.

--- src/test/string.cpp
+++ src/test/string.cpp
@@ -1,5 +1,5 @@
 //
-// Created by snizzfox on 6/26/21.
+// Created by Jim Marsden on 6/26/21.
 //
 
 #include "../../binary_search.hpp"
@@ -8,11 +8,29 @@
 #include <string>
 
 
-TEST_CASE("binary search string"){
+TEST_CASE("binary search - const string"){
+    using namespace std::string_literals;
+    auto const one_number{"a"s};
+    auto const string{"abcdefghijklmnop"s};
+    REQUIRE(mylib::find(one_number.begin(), one_number.end(), 'a') != one_number.end());
+    REQUIRE(mylib::find(one_number.begin(), one_number.end(), 'b') == one_number.end());
+    REQUIRE(mylib::find(string.begin(), string.end(), 'a') != string.end());
+
+    for(auto const & e : string){
+        REQUIRE(*mylib::find(string.begin(), string.end(), e) == e);
+    }
+}
+
+TEST_CASE("binary search - non const string"){
     using namespace std::string_literals;
     auto one_number{"a"s};
     auto string{"abcdefghijklmnop"s};
     REQUIRE(mylib::find(one_number.begin(), one_number.end(), 'a') != one_number.end());
     REQUIRE(mylib::find(one_number.begin(), one_number.end(), 'b') == one_number.end());
     REQUIRE(mylib::find(string.begin(), string.end(), 'a') != string.end());
+}
+
+TEST_CASE("binary search - empty string"){
+    std::string string{};
+    REQUIRE(mylib::find(string.begin(), string.end(), 'a') == string.end());
 }
